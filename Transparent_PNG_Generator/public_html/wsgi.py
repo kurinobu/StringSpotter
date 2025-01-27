984344ed60905650b96c6908845f7326b2d62930
kurinobu <110698077+kurinobu@users.noreply.github.com>
2025-01-27 11:56:53 +0900

    Delete Transparent_PNG_Generator/public_html directory

--- Transparent_PNG_Generator/public_html/wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '/home/users/2/main.jp-50364279a98534b5/web/tpg/Transparent_PNG_Generator')
-
-from public_html import app # public_htmlディレクトリからapp.pyをインポート
-
-application = app.app # Flaskアプリケーションのインスタンスを取得
-
-# 環境変数はここで設定しない！
